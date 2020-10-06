8852699904ff332ec29e530e339ca7819efe590b
RAHUL D <50445339+rahuldey613@users.noreply.github.com>
2020-10-06 16:40:53 +0530

    Add files via upload

---
+++ chunks/footer.php
@@ -0,0 +1,28 @@
+<section class="ffooter">
+		<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact US</h5>
+                <p class="grey-text text-lighten-4">123, Pramathnagar, Jamshedpur</p>
+                <p class="grey-text text-lighten-4">Phone : +21-562-896-5378</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Social Media Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Whatsapp</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2020 Copyright @ RahulDey
+            <a class="grey-text text-lighten-4 right" href="#!">Made in India with <span><i class="tiny material-icons">favorite</i></span></a>
+            </div>
+          </div>
+        </footer>
+	</section>
---
+++ chunks/reviews.php
@@ -0,0 +1,78 @@
+<section class="freviews">
+
+		<div class="section white center">
+			<h3 class="header">What Our Customers Say</h3>
+			<div class="carousel myreviews" style="margin-bottom: 35px;">
+			    <a class="carousel-item" href="#one!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Ramkesh Singh</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#two!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Rohan Roy</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#three!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Atul Chand</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#four!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Farhan Ahmed</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#five!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Riya Dutta</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#six!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Jasmine Sinha</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#seven!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Sangjukta Roy</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			  </div>
+		</div>
+	</section>
